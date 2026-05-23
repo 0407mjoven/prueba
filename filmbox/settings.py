8e91d06ddb6de41397b7c87663f9b247b7571460
0407mjoven <169162186+0407mjoven@users.noreply.github.com>
2026-05-23 14:59:28 +0200

    Update settings.py

--- filmbox/settings.py
+++ filmbox/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-p4*103sdl0h$!c0v9n=vo75+k=*lv_uc95j*xn$hi1(6kvp!37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['prueba-3umz.onrender.com']
 
 
 # Application definition
